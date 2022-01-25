322a6fd91f721fc0a6630d6ec152b139a01658db
Nassim1Omaati <2151890@talnet.nl>
2022-01-25 12:05:58 +0100

    opdracht afgemaakt

--- 03-Delete/Opdracht06-DELETE/antwoorden.sql
+++ 03-Delete/Opdracht06-DELETE/antwoorden.sql
@@ -1,9 +1,10 @@
 -- Opdracht 1 
-
+DELETE FROM players WHERE name = "David Silva";
 -- Opdracht 2 
-
+DELETE FROM players WHERE club = "willem II";
 -- Opdracht 3
-
+DELETE FROM players WHERE nationality = "belgium" AND club="fc barcelona"
 -- Opdracht 4
-
+DELETE FROM players WHERE age>37;
 -- Opdracht 5
+DELETE FROM players WHERE id=167905 OR id=169595
