204f137279584fc1f516237c8eae50c98e581dd7
Nassim1Omaati <2151890@talnet.nl>
2022-01-26 22:15:50 +0100

    opdr 3 verbeterd

--- 02-Select/Opdracht03-SELECT-WHERE/antwoorden.sql
+++ 02-Select/Opdracht03-SELECT-WHERE/antwoorden.sql
@@ -12,13 +12,11 @@ SELECT score FROM `jaar2016` WHERE country="Netherlands";
 -- Opdracht 5
 SELECT country , region FROM `jaar2015` WHERE region="North America";
 -- Opdracht 6 
-SELECT country, rank FROM `jaar2015` WHERE rank=25;
-
+SELECT country FROM `jaar2015`WHERE rank=25;
 -- Opdracht 7 
-SELECT country, score FROM `jaar2015` WHERE score>7087;
+SELECT country FROM `jaar2015` WHERE score>7087;
 -- Opdracht 8
-SELECT country, rank FROM `jaar2016` WHERE rank<=5;
-
+SELECT country, region FROM `jaar2016` WHERE rank<=5;
 -- Opdracht 9
 SELECT score FROM `jaar2015` WHERE country="Ireland";
 
