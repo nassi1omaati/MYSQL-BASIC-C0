b51727880fa9e6417d04e1b480a08fc472198e51
Nassim1Omaati <2151890@talnet.nl>
2022-01-26 18:58:55 +0100

    .

--- 02-Select/Opdracht04-AND-OR/antwoorden.sql
+++ 02-Select/Opdracht04-AND-OR/antwoorden.sql
@@ -7,25 +7,25 @@ SELECT * FROM `players` WHERE age="20" AND club = "Liverpool";
 -- Opdracht 4
 SELECT * FROM `players` WHERE nationality="Netherlands" AND club = "Ajax";
 -- Opdracht 5
-SELECT * FROM `players` WHERE club = 'ajax' AND nationality != 'Netherlands';
+SELECT * FROM `players` WHERE club = 'Ajax' AND nationality != 'Netherlands';
 -- Opdracht 6 
 SELECT name, age FROM `players` WHERE club="AZ Alkmaar";
 -- Opdracht 7 
 SELECT name, age, club FROM `players` WHERE club="AZ Alkmaar";
 -- Opdracht 8
-SELECT name, wage FROM `players` WHERE nationality="brazil" AND club="manchester city";
+SELECT name, wage FROM `players` WHERE nationality="Brazil" AND club="Manchester city";
 -- Opdracht 9
 SELECT name FROM `players` WHERE age=30 AND wage<=10000;
 -- Opdracht 10
 SELECT name, age FROM `players` WHERE nationality="Spain" or nationality="Portugal";
 -- Opdracht 11
-SELECT name, age, club FROM `players` WHERE nationality="portugal" OR club="chelsea";
+SELECT name, age, club FROM `players` WHERE nationality="Portugal" OR club="Chelsea";
 -- Opdracht 12
 SELECT name, age FROM `players` WHERE age>40 or wage=10000;
 -- Opdracht 13
-SELECT * FROM `players` WHERE nationality="Netherlands" AND club="ajax" OR club="FC Utrecht";
+SELECT * FROM `players` WHERE nationality="Netherlands" AND club="Ajax" OR club="FC Utrecht";
 
 -- Opdracht 14
-SELECT * FROM `players` WHERE nationality="england" AND age>20 OR wage=100000;
+SELECT * FROM `players` WHERE nationality="England" AND age>20 OR wage=100000;
 -- Opdracht 15
-SELECT name, age, nationality FROM `players` WHERE age=25 AND nationality="argentina" OR nationality="brazil";
+SELECT name, age, nationality FROM `players` WHERE age=25 AND nationality="Argentina" OR nationality="Brazil";
