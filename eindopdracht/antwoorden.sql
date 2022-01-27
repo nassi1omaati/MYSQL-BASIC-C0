1847130542825503b4cdb50bfacda608d0812cfa
Nassim1Omaati <2151890@talnet.nl>
2022-01-27 11:45:37 +0100

    .

--- eindopdracht/antwoorden.sql
+++ eindopdracht/antwoorden.sql
@@ -3,7 +3,7 @@ SELECT * FROM `videogamesales`;
 -- Opdracht 2 
 SELECT * FROM `videogamesales`WHERE year=1999;
 -- Opdracht 3
-SELECT `NA_Sales` FROM `videogamesales`WHERE genre='Sports';
+SELECT SUM(NA_Sales) FROM `videogamesales`WHERE genre='Sports';
 -- Opdracht 4
 SELECT name FROM `videogamesales` WHERE publisher = "Nintendo" AND year >= 1990 AND year <= 2005;
 -- Opdracht 5
@@ -13,15 +13,15 @@ SELECT AVG(EU_Sales) FROM `videogamesales` WHERE genre = 'puzzle'
 -- Opdracht 7 
 SELECT name, genre, publisher FROM `videogamesales` WHERE JP_Sales= 532;
 -- Opdracht 8
-SELECT SUM(Global_Sales) FROM `videogamesales` WHERE publisher="Nintendo";
+SELECT Count(Global_Sales) FROM `videogamesales` WHERE publisher="Nintendo";
 -- Opdracht 9
-SELECT name, year FROM `videogamesales` WHERE genre= 'racing' AND publisher= "nintendo" OR publisher= "activision";
+SELECT name, year FROM `videogamesales` WHERE genre = 'racing' AND (publisher= "nintendo" OR publisher = "activision")
 -- Opdracht 10
 SELECT AVG(`JP_Sales`), AVG(`NA_Sales`), AVG(`EU_Sales`) AS average_sales_jp_eu_na FROM `videogamesales`;
 -- Opdracht 11
 DELETE  FROM `videogamesales` WHERE name="halo 2";
 -- Opdracht 12
-DELETE  FROM `videogamesales` WHERE year = 2012 OR publisher = "ubisoft"
+DELETE  FROM `videogamesales` WHERE year = 2012 AND publisher = "ubisoft"
 -- Opdracht 13
 DELETE  FROM `videogamesales` WHERE genre = "Adventure"  AND publisher = "activison"
 -- Opdracht 14
